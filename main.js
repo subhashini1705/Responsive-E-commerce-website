8fcd1e48a1071eae718088f01e3391bee27c25b1
subhashini1705 <98764870+subhashini1705@users.noreply.github.com>
2025-02-15 12:19:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+//Header Scroll Color Change
+let header=document.querySelector('header');
+
+window.addEventListener('scroll',() =>{
+    header.classList.toggle('shadow', window.scrollY >0);
+});
+//Menu
+let menu=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menu.onclick= () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+//Remove Menu on scroll
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
